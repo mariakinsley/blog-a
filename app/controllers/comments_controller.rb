bc79414d3b49fac8751d4fa34e6825c4e948be4b
Maria Kinsley <mariadfrancia2016@gmail.com>
2017-04-30 16:22:56 -0400

    add pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,10 @@ class CommentsController < ApplicationController
     @post =@post.comments.create(params[:comment].permit(:name, :body))
     redirect_to post_path(@post)
   end
+  def destroy
+      @post =Post (params[:post_id])
+      @comments =@post.comments.find(params[:id])
+      @comment.destroy
+      redirect_to post_path(@)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, lenght: {minimum:5}
   validates :body, presence: true
 end
